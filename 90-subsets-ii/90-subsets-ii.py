ce00897e98879286daf892b25e3084fc2b899b15
Shyam_Jain <78031635+Shyamjain2001@users.noreply.github.com>
2022-05-14 23:21:04 +0530

    Time: 121 ms (5.24%), Space: 14.2 MB (49.88%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,18 @@
+import copy
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        op,res=[],[]
+        def go(nums,op,res):
+            if len(nums)==0:
+                op.sort()
+                if op not in res:
+                    res.append(op)
+                return 
+            op1=copy.deepcopy(op)
+            op2=copy.deepcopy(op)
+            op1.append(nums[0])
+            go(nums[1:],op1,res)
+            go(nums[1:],op2,res)
+            
+        go(nums,op,res)
+        return res
