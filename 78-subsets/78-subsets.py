872f2df35504e593a90ca2eeec51df09b6de3f32
Shyam_Jain <78031635+Shyamjain2001@users.noreply.github.com>
2022-05-14 19:57:25 +0530

    Time: 39 ms (73.86%), Space: 14.1 MB (81.39%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,26 +1,21 @@
-import copy
-class Solution(object):
-    def solver_helper(self,input, output,result):
-        if (len(input)==0):
-            #at leaf node append to output
-            result.append(output)
-            return
 
-        op1=copy.deepcopy(output)
-        op2=copy.deepcopy(output)
-        op2.append(input[0])
-        self.solver_helper(input[1:],op1,result)
-        self.solver_helper(input[1:],op2,result)
+import copy
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        op,res=[],[]
+        def go(nums,op,res):
+            if len(nums)==0:
+                res.append(op)
+                return
+            op1=copy.deepcopy(op)
+            op2=copy.deepcopy(op) 
+            op1.append(nums[0])
             
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-               
-            
-        input=nums
-        result,output=[],[]
-        self.solver_helper(input, output,result)
-        print(result)
-        return result
+            #print(op1)
+            #print(op2)
+            #print(nums)
+            go(nums[1:],op2,res)
+            go(nums[1:],op1,res)
+        go(nums,op,res)
+        return res
+        
