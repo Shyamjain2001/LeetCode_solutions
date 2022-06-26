5b927608685dd4ddf5deb7041e46fdfe621482f6
Shyam_Jain <78031635+Shyamjain2001@users.noreply.github.com>
2022-06-26 17:23:41 +0530

    Added solution - LeetHub

---
+++ 9. Gray to Binary equivalent - GFG/9.-gray-to-binary-equivalent.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:    
+    ##Complete this function
+    # function to convert a given Gray equivalent n to Binary equivalent.
+    def grayToBinary(self,n):
+        ##Your code here
+        op=n
+        while n:
+            n>>=1
+            op=op^n 
+        return op
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        print(ob.grayToBinary(n))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
